73d4a6ea1c0f980a70d37679da3b2c58aad693b9
tusharburai <tusharburai@gmail.com>
2025-09-02 20:05:49 +0530

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,34 +1,27 @@
-// portfolio Page //
+// Smooth scroll for menu clicks
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href');
+    document.querySelector(targetId).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
 // Contact form validation + message
 document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-  
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
-    const formMessage = document.getElementById("formMessage");
-  
-    if (name === "" || email === "" || message === "") {
-      formMessage.textContent = "Please fill out all fields.";
-      formMessage.style.color = "red";
-    } else {
-      formMessage.textContent = "✅ Your message has been sent successfully!";
-      formMessage.style.color = "lightgreen";
-      this.reset();
-    }
-  });
-  
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  
-      if (!storedUser) {
-        // No user → go back to login
-        window.location.href = "login.html";
-      } else {
-        document.getElementById("username").textContent = storedUser.name;
-      }
-  
-      document.getElementById("logoutBtn").addEventListener("click", () => {
-        localStorage.removeItem("user");
-        window.location.href = "login.html";
-            });
-  
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
+  const formMessage = document.getElementById("formMessage");
+
+  if (name === "" || email === "" || message === "") {
+    formMessage.textContent = "Please fill out all fields.";
+    formMessage.style.color = "red";
+  } else {
+    formMessage.textContent = "✅ Your message has been sent successfully!";
+    formMessage.style.color = "lightgreen";
+    this.reset();
+  }
+});
